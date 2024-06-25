eec8d02423753d5fe41141eb97894a5aa6bb4c0b
ragendhu <ragendhu@DESKTOP-0F8M4MR>
2024-06-25 19:05:25 +0530

    first commit

---
+++ corejava/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,15 @@
+//program to demonstrate encapsulation
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialNumber;// data variables
+	String name;
+	int age;
+	
+	void show()//member function
+	{
+		System.out.println("serialNumber:"+serialNumber+"\t Name:"+name +"\t Age:"+age);
+	}
+	
+
+}
---
+++ corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.encapsulation;
+
+public class EncapsulationDemo {
+	
+	public static void main(String args[])
+	{
+	Encapsulation ob=new Encapsulation();
+	
+	ob.name="Ragendhu";
+	ob.age=21;
+	ob.serialNumber=111;
+	ob.show();
+	}		
+
+}
