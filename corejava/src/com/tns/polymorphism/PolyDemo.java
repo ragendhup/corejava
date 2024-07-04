68c5d53b257c083dc51ff806e156fd4e8741a818
ragendhu <ragendhu@DESKTOP-0F8M4MR>
2024-07-04 19:34:36 +0530

    first commit

---
+++ corejava/src/com/tns/polymorphism/PolyDemo.java
@@ -0,0 +1,15 @@
+package com.tns.polymorphism;
+
+public class PolyDemo {
+	public static void main(String args[]) {
+		Point pone=new Point();
+		System.out.println(pone);
+		Point ptwo=new Point(2.3f,8.9f);
+		System.out.println(ptwo);
+	}
+public static void main(float z) {
+	Point pthree=new Point(9.0f);
+	System.out.println(pthree);
+	
+}
+}
